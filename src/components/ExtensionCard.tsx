6a19b73fa7d52b240ac8e56bbab41fa3ff212c1c
Nagy Attila <n.atisulis10@gmail.com>
2025-09-09 14:03:51 +0200

    asd

--- src/components/ExtensionCard.tsx
+++ src/components/ExtensionCard.tsx
@@ -1,9 +1,21 @@
+import ToggleSwitch from "./ToggleSwitch";
+
 function ExtensionCard(){
 
 
     return(
-        <div>
-            
+        <div className="extensionCard">
+            <div>
+                <div className="icon">❤️</div>
+                <div>
+                    <h3>DevLens</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam, praesentium.</p>
+                </div>
+            </div>
+            <div>
+                <button>Remove</button>
+                <ToggleSwitch/>
+            </div>
         </div>
     )
 }
